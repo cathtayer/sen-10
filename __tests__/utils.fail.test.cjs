1e01a0cd5abeb66254c36ec6c4d571ff9bb08674
cathtayer <catherinetayer25@gmail.com>
2025-11-05 13:59:08 +0800

    revert

--- __tests__/utils.fail.test.cjs
+++ __tests__/utils.fail.test.cjs
@@ -2,5 +2,5 @@ const { add } = require('../src/utils.cjs');
 
 test('intentional failing test: 2 + 2 equals 5 (failing test)', () => {
   // This test is intentionally wrong to demonstrate a failing test
-  expect(add(2, 2)).toBe(4);
+  expect(add(2, 2)).toBe(5);
 });
